d74e2eee2811861da89721ef8c134c7fc27d4719
Johongir10 <shavkatovj73@gmail.com>
2024-07-31 10:15:52 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,23 @@
+// let son=+prompt("1/dan5 gacha son kiriting").toLocaleLowerCase().trim();
+
+// switch(son){
+//     case 1:alert("  Juda Yomon");break; 
+//     case 2:alert("Yomon");break;
+//     case 3:alert("Qoniqarli");break;
+//     case 4:alert("Yaxshi");break;
+//     case 5:alert("Alo");break;
+//     default: alert("Bunday son yoq");break;
+    
+// }
+
+let person={
+
+}
+
+person.Ismingiz=Ism= prompt("Ismingizni Kiriting");
+person.spaceid=ID= prompt("space id kiriting");
+person.spacepassword=  parol=prompt("space password kiriting");
+
+
+
+console.log(person);
